9134f2e64353b09817b7e38940f34375c2b88350
rozzy <rosekaremeri@gmail.com>
2024-10-04 00:21:30 +0300

    Fix: Verification, that works yay

--- ludo_pseudorandom/hardhat.config.ts
+++ ludo_pseudorandom/hardhat.config.ts
@@ -4,6 +4,7 @@ import "@nomicfoundation/hardhat-toolbox";
 require('dotenv').config();
 
 const config: HardhatUserConfig = {
+
   solidity: "0.8.23",
   networks: {
     // for testnet
@@ -12,6 +13,26 @@ const config: HardhatUserConfig = {
       accounts: [process.env.WALLET_KEY as string],
       gasPrice: 1000000000,
     },
+  },
+  // Hardhat expects etherscan here, even if you're using Blockscout.
+  etherscan: {
+    // Use "123" as a placeholder, because Blockscout doesn't need a real API key, and Hardhat will complain if this property isn't set.
+    apiKey: {
+      "lisk-sepolia": "1AT3KKRJAWAJSKGCMRW1HAN2T4EA9387B7"
+    },
+    customChains: [
+      {
+          network: "lisk-sepolia",
+          chainId: 4202,
+          urls: {
+              apiURL: "https://sepolia-blockscout.lisk.com/api",
+              browserURL: "https://sepolia-blockscout.lisk.com"
+          }
+      }
+    ]
+  },
+  sourcify: {
+    enabled: false
   },
 };
 
