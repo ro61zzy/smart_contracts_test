1ed7c0ed265535a718ad7a6b0603963de9f571f7
rozzy <rosekaremeri@gmail.com>
2024-10-04 12:58:28 +0300

    deploy

--- ludo_pseudorandom/ignition/modules/deploy.ts
+++ ludo_pseudorandom/ignition/modules/deploy.ts
@@ -1,6 +1,14 @@
 import { ethers } from 'hardhat';
 
 async function main() {
+    const simpleLudo = await ethers.deployContract('SimpleLudo');
+
+    await simpleLudo.waitForDeployment();
+
+    console.log('SimpleLudo Contract Deployed at ' + simpleLudo.target);
+
+
+    //student Portal one
     const studentPortal = await ethers.deployContract('studentPortal');
 
     await studentPortal.waitForDeployment();
